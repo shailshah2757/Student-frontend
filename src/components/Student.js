99b2c2b4bf26b950be363d0f33ee52e193d91996
20it135 <80442908+shailshah2757@users.noreply.github.com>
2022-09-14 15:43:13 +0530

    Added Alert Toast

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,118 +1,74 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Container, Form, Card, Button } from "react-bootstrap";
-import { Navigate, useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {Link} from 'react-router-dom'
 
-export default function Student() {
-  const [id, setId] = useState(null);
-  const [name, setName] = useState(null);
-  const [address, setAddress] = useState(null);
-
-  const { studentId } = useParams(); // Get the Path Parameter from the URL
-  const navigate = useNavigate();
+export default function StudentList(props) {
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    if (studentId) {
-      axios
-        .get("http://localhost:8080/student/" + studentId)
-        .then((response) => {
-          if (response.data != null) {
-            setId(response.data.id);
-            setName(response.data.name);
-            setAddress(response.data.address);
-          }
-        })
-        .catch((error) => alert(error));
-    }
+    getStudents();
   }, []);
 
-  let student = {
-    id: id,
-    name: name,
-    address: address,
+  let getStudents = () => {
+    axios
+      .get("http://localhost:8080/listStudents")
+      .then((response) => setStudents(response.data))
+      .catch((error) => alert(error));
   };
 
-  let textChanged = (event) => {
-    if (event.target.name === "id") {
-      setId(event.target.value);
-    } else if (event.target.name === "name") {
-      setName(event.target.value);
-    } else if (event.target.name === "address") {
-      setAddress(event.target.value);
-    }
-  };
-
-  let saveStudent = (event) => {
-    event.preventDefault();
-    
-      axios
-        .post("http://localhost:8080/student", student)
-        .then((response) => {
-          if (response.data != null) {
-            alert("Record added successfully");
-          }
-        })
-        .catch((error) => alert(error));
-  };
-
-  let updateStudent = (event) => {
-    event.preventDefault();
-    axios.put("http://localhost:8080/student/" + studentId, student).then((response) => {
-      if (response.data != null) {
-        alert("Record Updated successfully");
-        navigate("/listStudents"); // Navigate to Students List Components
+  let deleteStudent = (studentId) => {
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        props.showAlert("success", "Record deleted successfully")
+        setStudents(students.filter(student=>student.id!==studentId));
       }
-    });
-  };
+    })
+  }
 
   return (
     <div className="my-3">
       <Container>
-        <Card>
-          <Form onSubmit={studentId != null ? updateStudent : saveStudent}>
-            <Card.Header>
-              <strong>{studentId!=null? "Update Student Information":"Add Student Information"}</strong>
-            </Card.Header>
-            <Card.Body>
-              <Form.Group className="mb-3">
-                <Form.Label>Id</Form.Label>
-                <Form.Control
-                  name="id"
-                  value={id}
-                  type="text"
-                  placeholder="Enter id"
-                  onChange={textChanged}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  name="name"
-                  value={name}
-                  type="text"
-                  placeholder="Enter name"
-                  onChange={textChanged}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                  name="address"
-                  value={address}
-                  type="text"
-                  placeholder="Enter address"
-                  onChange={textChanged}
-                />
-              </Form.Group>
-            </Card.Body>
-            <Card.Footer>
-              <Button variant="primary" type="submit">
-                {studentId != null ? "Update" : "Submit"}
-              </Button>
-            </Card.Footer>
-          </Form>
-        </Card>
+        <Card.Header>
+          <h3>Students List</h3>
+        </Card.Header>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Student Id</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
+                <th>Edit/Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.length === 0 ? (
+                <tr>
+                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                </tr>
+              ) : (
+                students.map((student)=>
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Link to={"/student/"+student.id}><Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button></Link>{ ' '}
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                )
+              )}
+            </tbody>
+          </Table>
+        </Card.Body>
       </Container>
     </div>
   );
